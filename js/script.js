e29e8b3bf63b8a49368d69909b0ca72ab3eefc75
Fernando Araujo <jfac2420@gmail.com>
2025-04-06 20:02:45 -0500

    Add comment

--- js/script.js
+++ js/script.js
@@ -1,3 +1,3 @@
-document.getElementById('clickMe').addEventListener('click', function() {
-    document.getElementById('message').textContent = 'Button clicked!';
+document.getElementById('clickMe').addEventListener('click', function() { // Agrega un evento de clic al botón con id 'clickMe'
+    document.getElementById('message').textContent = 'Button clicked!'; // Cambia el contenido del párrafo con id 'message' al hacer clic
 });
